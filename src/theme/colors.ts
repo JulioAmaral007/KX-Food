5cbe7114e7849ac78db173d03639202877eaec69
Júlio Cézar <jczrpereira77@gmail.com>
2024-05-12 18:44:37 -0300

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  red: "#EA1D2C",
+  yellow: "#FFB100",
+  green: "#5DC05B",
+
+
+  gray: {
+    100: "#F4F4F5",
+    200: "#EEEEEE",
+    300: "#7E8392",
+    400: "#323232",
+  },
+}
